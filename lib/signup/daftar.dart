3397be739fb8e90aec3cd46bf025a8be548c3190
DESKTOP-F01RJJB\ASUS <enggalcahyop@gmail.com>
2022-07-10 08:17:10 +0700

    halaman sign up

---
+++ lib/signup/daftar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class daftar extends StatefulWidget {
+  const daftar({Key? key}) : super(key: key);
+
+  @override
+  State<daftar> createState() => _daftarState();
+}
+
+class _daftarState extends State<daftar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Text("Halaman Sign Up")
+            ],
+          ),
+        )),
+    );
+  }
+}
