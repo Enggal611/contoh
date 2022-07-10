6bd81ba93f86ca2bc5c67a4d1f87be7f9a20f799
DESKTOP-F01RJJB\ASUS <enggalcahyop@gmail.com>
2022-07-10 08:12:49 +0700

    halaman login

---
+++ lib/login/login.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class login extends StatefulWidget {
+  const login({Key? key}) : super(key: key);
+
+  @override
+  State<login> createState() => _loginState();
+}
+
+class _loginState extends State<login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Text("Halaman Login")
+            ],
+          ),
+        )),
+    );
+  }
+}
